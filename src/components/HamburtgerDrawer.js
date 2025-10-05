451e69e6baadb8a9e0fe89a6c66e60e19ed03772
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-10-05 21:45:30 +0530

    Inshort Clone React JS

--- src/components/HamburtgerDrawer.js
+++ src/components/HamburtgerDrawer.js
@@ -11,76 +11,101 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme,  ThemeProvider } from "@mui/material/styles";
+import clsx from "clsx";
+import { makeStyles } from '@mui/styles';
 
-export default function HamburtgerDrawer() {
-  const [state, setState] = React.useState({
-    
-    left: false,
+
+const useStyles = makeStyles({
+    list: {
+        width: 200,
+        paddingLeft: 10,
+        paddingRight: 5,
+    },
+    fullList: {
+        width: "auto",
+    },
 });
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event &&
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
-      return;
-    }
 
-    setState({ ...state, [anchor]: open });
-  };
+export default function HamburtgerDrawer() {
+    const classes = useStyles()
+    const [state, setState] = React.useState({
 
-  const list = (anchor) => (
-    <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+        left: false,
+    });
 
-  return (
-    <div>
-     <React.Fragment key={"left"}>
-          <Button onClick={toggleDrawer("left", true)}>
-            <MenuIcon />
-          </Button>
-          <SwipeableDrawer
-            anchor={"left"}
-            open={state["left"]}
-            onClose={toggleDrawer("left", false)}
-            onOpen={toggleDrawer("left", true)}
-          >
-            {list("left")}
-          </SwipeableDrawer>
-        </React.Fragment>
-    </div>
-    
-  );
+    const theme = createTheme({
+        palette: { mode: "dark" },
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+        className={clsx(classes.list, {
+            [classes.fullList]: anchor === "top" || anchor === "Button",
+        })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            <React.Fragment key={"left"}>
+                <Button onClick={toggleDrawer("left", true)}>
+                    <MenuIcon />
+                </Button>
+                <ThemeProvider theme={theme}>
+                    <SwipeableDrawer
+                        anchor={"left"}
+                        open={state["left"]}
+                        onClose={toggleDrawer("left", false)}
+                        onOpen={toggleDrawer("left", true)}
+                    >
+                        {list("left")}
+                    </SwipeableDrawer>
+                </ThemeProvider>
+            </React.Fragment>
+        </div>
+
+    );
 }
